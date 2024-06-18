80af705140a84b2c6d64b2e1c1e6982c4e42bacb
Lucas Katacha <lucasgabrieljaci19@gmail.com>
2024-06-18 16:58:19 -0300

    'auth-corrigido-2.0'

--- src/app/finishSignUp/page.jsx
+++ src/app/finishSignUp/page.jsx
@@ -1,12 +1,11 @@
 // pages/finishSignUp.js
 'use client'
-
-import { Suspense, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLogin } from '@/hooks/useLogin'; // Certifique-se de que o caminho para useLogin está correto
 import { useRouter, useSearchParams } from 'next/navigation';
 import ToastManager from '@/components/ToastManager';
 
-const FinishSignUp = ({ searchParams }) => {
+export default function FinishSignUp({ searchParams }) {
     const { completeLogin, error, isPending } = useLogin();
     const navigation = useRouter();
 
@@ -21,16 +20,16 @@ const FinishSignUp = ({ searchParams }) => {
     }
 
     useEffect(() => {
-        const authKey = searchParams.get('authKey');
+        const authKey = searchParams.authKey;
 
         const handleCompleteLogin = async () => {
             try {
-                addToast('Analisando Token', 'info');
+                addToast('Analizando Token', 'info')
                 const completed = await completeLogin(authKey);
-                addToast('Token Aprovado', 'success');
+                addToast('Token Aprovado', 'success')
                 navigation.push(completed);
             } catch (err) {
-                addToast('Token Inválido', 'error');
+                addToast('Token Invalido', 'error')
                 console.error('Erro ao completar login:', err);
                 navigation.push('/');
             }
@@ -39,7 +38,7 @@ const FinishSignUp = ({ searchParams }) => {
         if (authKey) {
             handleCompleteLogin();
         }
-    }, [searchParams]);
+    }, []);
 
     return (
         <div>
@@ -49,21 +48,4 @@ const FinishSignUp = ({ searchParams }) => {
             </h1>
         </div>
     );
-};
-
-const FinishSignUpWithSuspense = () => {
-    const searchParams = useSearchParams();
-    return (
-        <Suspense
-            fallback={
-                <h1 className='bg-[#0B060F] w-full h-screen text-white font-bold text-3xl titleForm text-center flex justify-center items-center'>
-                    <p>Carregando...</p>
-                </h1>
-            }
-        >
-            <FinishSignUp searchParams={searchParams} />
-        </Suspense>
-    );
-};
-
-export default FinishSignUpWithSuspense;
+}
